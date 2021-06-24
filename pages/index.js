92ac4d1f7b6767f94342660036b2ee5c3e8836e5
jacobhq <jacob.marshall.jem@gmail.com>
2021-06-24 07:35:17 +0000

    Verify on complete

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Head from 'next/head'
 import { Center, Heading, Text, PinInput, PinInputField, HStack, Button } from "@chakra-ui/react"
+import { useState } from 'react'
 
 export default function Home() {
-  var loading = false
-
-  function load () {
-      loading = true
-      console.log(loading)
-  }
+  let [loading, setLoading] = useState(false)
+  let startLoader = () => setLoading(true)
 
   return (
       <Center w="100vw" h="100vh" style={{flexDirection: 'column'}}>
@@ -15,7 +12,7 @@ export default function Home() {
           <Text>We sent a code to your phone</Text>
           <br />
           <HStack spacing="8px">
-            <PinInput otp type="alphanumeric" onComplete={(() => load())}>
+            <PinInput otp type="alphanumeric" onComplete={startLoader} isDisabled={loading}>
                 <PinInputField />
                 <PinInputField />
                 <PinInputField />
@@ -23,7 +20,7 @@ export default function Home() {
             </PinInput>
           </HStack>
           <br />
-          <Button isLoading={loading} loadingText="Verifying" colorScheme="teal" onClick={load()}>Verify</Button>
+          <Button isLoading={loading} loadingText="Verifying" colorScheme="teal" onClick={startLoader}>Verify</Button>
       </Center>
   )
 }
