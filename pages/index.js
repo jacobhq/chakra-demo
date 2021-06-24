16ea58be44855a14b0cf87079bcca20e8d05cbb0
jacobhq <jacob.marshall.jem@gmail.com>
2021-06-24 09:30:46 +0000

    New var names

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
 import Head from 'next/head'
-import { Center, Heading, Text, PinInput, PinInputField, HStack, Button } from "@chakra-ui/react"
+import { Center, Heading, Text, PinInput, PinInputField, HStack, Button, Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react"
 import { useState } from 'react'
 
 export default function Home() {
-  let [loading, setLoading] = useState(false)
-  let startLoader = () => setLoading(true)
+  let [verifying, setVerifying] = useState(false)
+  let startVerifying = () => setVerifying(true)
 
   return (
-      <Center w="100vw" h="100vh" style={{flexDirection: 'column'}}>
-          <Heading style={{marginBottom: '5px'}}>Verify yourself</Heading>
-          <Text>We sent a code to your phone</Text>
-          <br />
-          <HStack spacing="8px">
-            <PinInput otp type="alphanumeric" onComplete={startLoader} isDisabled={loading}>
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-            </PinInput>
-          </HStack>
-          <br />
-          <Button isLoading={loading} loadingText="Verifying" colorScheme="teal" onClick={startLoader}>Verify</Button>
-      </Center>
+    <Center w="100vw" h="100vh" style={{ flexDirection: 'column' }}>
+      <Heading style={{ marginBottom: '5px' }}>Verify yourself</Heading>
+      <Text>We sent a code to your phone</Text>
+      <br />
+      <HStack spacing="8px">
+          <PinInput otp type="alphanumeric" onComplete={startVerifying} isDisabled={verifying}>
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+          </PinInput>
+      </HStack>
+      <br />
+      <Button isverifying={verifying} loadingText="Verifying" colorScheme="teal" onClick={startVerifying}>Verify</Button>
+    </Center>
   )
 }
